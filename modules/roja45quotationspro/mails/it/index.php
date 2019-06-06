4ad155bfe9eab5c369fb9e6ff19958065b607a76
Edwin Marte <egm0815@gmail.com>
2019-06-06 12:12:13 -0400

    Add the web standard mail format at header and footer

---
+++ modules/roja45quotationspro/mails/it/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *  2016 ROJA45.COM
+ *  All rights reserved.
+ *  
+ *  DISCLAIMER
+ *  
+ *  Changing this file will render any support provided by us null and void.
+ *  
+ *  @author 			Roja45 <support@roja45.com>
+ *  @copyright  		2016 roja45.com
+*/
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
