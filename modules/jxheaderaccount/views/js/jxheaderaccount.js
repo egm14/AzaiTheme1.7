558142079eda4740587d7715ac878f3708a66c3d
Edwin Marte <egm0815@gmail.com>
2020-03-11 14:59:40 -0400

    Activate the relink to /module/idxvalidatinguser/deluxevalidatinguser when azaiwholesalee is locked

--- modules/jxheaderaccount/views/js/jxheaderaccount.js
+++ modules/jxheaderaccount/views/js/jxheaderaccount.js
@@ -242,11 +242,11 @@ function submitCreate(elem){
           
           
 
-          if(shopName == "azaimayoreo2"){
+          if(shopName == "azaimayoreo"){
                       uripage = prestashop.urls.base_url + prestashop.language.iso_code; 
                       setTimeout(function(){
-                     //window.location.assign(uripage +"/module/idxvalidatinguser/deluxevalidatinguser")
-                      window.location.href = prestashop.urls.pages.authentication;
+                      window.location.assign(uripage +"/module/idxvalidatinguser/deluxevalidatinguser")
+                      //window.location.href = prestashop.urls.pages.authentication;
                       $(".loader-page").show();
                       },3000)
                
